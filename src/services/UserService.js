9630c4b9d66d83a97a03945033b981fc7beafffd
Jonas Sebastian <jonas.sebastian@dlsau.edu.ph>
2024-09-27 00:18:30 +0800

    Update UserService for new API calls

--- src/services/UserService.js
+++ src/services/UserService.js
@@ -2,13 +2,25 @@ import axios from 'axios';
 import { API_URL_REGISTER, API_URL_LOGIN } from './ApiService';
 
 class UserService {
-  registerUser(data) {
-    return axios.post(API_URL_REGISTER, data);
-  }
+    registerUser(data) {
+        return axios.post(API_URL_REGISTER, data);
+    }
 
-  loginUser(data) {
-    return axios.post(API_URL_LOGIN, data);
-  }
+    loginUser(data) {
+        return axios.post(API_URL_LOGIN, data);
+    }
+
+    getAllUsers() {
+        return axios.get('/api/shop-admin/admin-users/');
+    }
+
+    deleteUser(userId) {
+        return axios.delete(`/api/shop-admin/admin-users/${userId}/`);
+    }
+
+    updateUser(userId, data) {
+        return axios.put(`/api/shop-admin/admin-users/${userId}/`, data);
+    }
 }
 
 const userServiceInstance = new UserService();
