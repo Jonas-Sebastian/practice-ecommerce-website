1b2c9ba9388069c8ecb40c341167de80d85b6abc
Jonas Sebastian <jonas.sebastian@dlsau.edu.ph>
2024-10-19 04:34:06 +0800

    Add 1366px width breakpoint

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,11 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        'lt-1366': { 'max': '1366px' },
+      },
+    },
   },
   plugins: [],
 };
